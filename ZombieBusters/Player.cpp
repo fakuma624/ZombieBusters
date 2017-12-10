dbc1a97710674839e6a8c48c1037a43e4e1042f4
永海　拓真 <1031fakuma@gmail.com>
2017-12-11 05:24:40 +0900

    [#10 enh]移動アニメーション実装　（プレイヤーアニメーション残り攻撃だけど攻撃はアニメーションないけどどうするんだろ）

--- ZombieBusters/Player.cpp
+++ ZombieBusters/Player.cpp
@@ -12,6 +12,10 @@ Player::Player()
 	 Size.Hight=180.0f;
 	 Speed=2.0f;
 	 Direction = RIGHT;
+	 IsAtk = false;
+	 IsMoving = false;
+	 MovementX = 0;
+	 MovementY = 0;
 }
 
 
@@ -38,38 +42,121 @@ void Player::Draw()
 		player[i].y += WindowPos.y;
 
 	}
-	static int Fcount = 0;
-
-	if (Fcount >= 30 && Fcount < 60)
+	static int StayCount = 0;
+	static int MoveCount = 0;
+	if (IsMoving == false)
 	{
-		for (int i = 0; i < 4; ++i)
+		
+		static int const StayAnimationFreamMax = 60;
+
+		if (StayCount >= StayAnimationFreamMax/2 && StayCount < StayAnimationFreamMax)
 		{
-			player[i].tu += PlayerTu;
+			for (int i = 0; i < 4; ++i)
+			{
+				player[i].tu += PlayerTu;
+
+			}
+		}
+		if (Direction == LEFT)
+		{
+
+		}
+		if (Direction == RIGHT)
+		{
+			//”½“]‚·‚éŠÖ”‚ðŒÄ‚Ô
+			DirectGraphics::GetpInstance()->InvertedRight(player);
+		}
+		if (StayCount == 60)
+		{
+			StayCount = 0;
+		}
+		++StayCount;
+	}
+	if (IsMoving)
+	{
+	
+		static int const MoveAnimationFreamCount = 120;
+		if (0 <= MoveCount && MoveCount < MoveAnimationFreamCount * 1 / 4)
+		{
+			for (int i = 0; i < 4; ++i)
+			{
+				player[i].tu += PlayerTu * 0;
+				player[i].tv += PlayerTv * 1;
+
+			}
+		}
+		if (MoveAnimationFreamCount * 1 / 4 <= MoveCount && MoveCount < MoveAnimationFreamCount * 2 / 4)
+		{
+			for (int i = 0; i < 4; ++i)
+			{
+				player[i].tu += PlayerTu * 1;
+				player[i].tv += PlayerTv * 1;
+
+			}
+		}
+		if (MoveAnimationFreamCount * 2 / 4 <= MoveCount && MoveCount < MoveAnimationFreamCount * 3 / 4)
+		{
+			for (int i = 0; i < 4; ++i)
+			{
+				player[i].tu += PlayerTu * 2;
+				player[i].tv += PlayerTv * 1;
+
+			}
+		}
+		if (MoveAnimationFreamCount * 3 / 4 <= MoveCount && MoveCount < MoveAnimationFreamCount * 4 / 4)
+		{
+			for (int i = 0; i < 4; ++i)
+			{
+				player[i].tu += PlayerTu * 1;
+				player[i].tv += PlayerTv * 1;
+
+			}
+		}
+
+		
+		if (MoveCount == MoveAnimationFreamCount)
+		{
+			MoveCount = 0;
+		}
+		++MoveCount;
+
+		if (Direction == LEFT)
+		{
+
+		}
+		if (Direction == RIGHT)
+		{
+			//”½“]‚·‚éŠÖ”‚ðŒÄ‚Ô
+			DirectGraphics::GetpInstance()->InvertedRight(player);
+
+
+
+
 
 		}
 	}
-	if (Direction == LEFT)
+	if (IsMoving)
 	{
-
+		StayCount = 0;
 	}
-	if (Direction == RIGHT)
+	else
 	{
-		//”½“]‚·‚éŠÖ”‚ðŒÄ‚Ô
-		DirectGraphics::GetpInstance()->InvertedRight(player);
+		MoveCount = 0;
 	}
-	if (Fcount == 60)
-	{
-		Fcount = 0;
-	}
-
-
 
 	DirectGraphics::GetpInstance()->Draw(&TextureID, player);
-	++Fcount;
+
 }
+
 void Player::Update()
 {
-	/*KEYSTATE* key = DirectInput::GetInstance()->GetKey();*/
+	Control();
+	Move();
+
+}
+
+void Player::Control()
+{
 	DirectInput::GetInstance()->KeyCheck(&Key[KEY_LEFT], DIK_LEFT);
 	DirectInput::GetInstance()->KeyCheck(&Key[KEY_RIGHT], DIK_RIGHT);
 	DirectInput::GetInstance()->KeyCheck(&Key[KEY_UP], DIK_UP);
@@ -79,19 +166,53 @@ void Player::Update()
 	DirectInput::GetInstance()->KeyCheck(&Key[KEY_W], DIK_W);
 	DirectInput::GetInstance()->KeyCheck(&Key[KEY_SPACE], DIK_SPACE);
 
+	if (MovementX != 0 || MovementY != 0)
+	{
+		MovementX = 0;
+		MovementY = 0;
+	}
 
 	if (Key[KEY_UP] == KEY_ON) {
-		WindowPos.y -= Speed;
+		MovementY = -Speed;
 	}
 	if (Key[KEY_DOWN] == KEY_ON) {
-		WindowPos.y += Speed;
+		MovementY = +Speed;
 	}
 	if (Key[KEY_LEFT] == KEY_ON) {
-		WindowPos.x -= Speed;
+		MovementX = -Speed;
 		Direction = LEFT;
 	}
 	if (Key[KEY_RIGHT] == KEY_ON) {
-		WindowPos.x += Speed;
+		MovementX = +Speed;
 		Direction = RIGHT;
 	}
+
+	if (MovementX != 0 || MovementY != 0)
+	{
+		IsMoving = true;
+	}
+	else
+	{
+		IsMoving = false;
+	}
+
+	if (Key[KEY_Z] == KEY_ON) {
+		IsAtk = true;
+	}
+	if (IsAtk)
+	{
+		static int AtkFcount = 0;
+		++AtkFcount;
+		if (AtkFcount == 40)
+		{
+			IsAtk = false;
+		}
+	}
+}
+
+void Player::Move()
+{
+	WindowPos.x += MovementX;
+	WindowPos.y += MovementY;
+
 }
