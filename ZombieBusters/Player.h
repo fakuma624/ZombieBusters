062691fa32bc7066377c7e577e3e35ec90cb2b2e
永海　拓真 <1031fakuma@gmail.com>
2017-12-11 04:31:18 +0900

    ｃｘｃｘ

--- ZombieBusters/Player.h
+++ ZombieBusters/Player.h
@@ -23,29 +23,12 @@ private:
 	D3DXVECTOR2 WindowPos;
 	D3DXVECTOR2 WorldPos;
 	TEXTURESIZE Size;
-<<<<<<< HEAD
-	float Speed;
-
-	Direction Direction;
-	const float PlayerTu = 90.f / 1024.f;
-	const float PlayerTv = 180.f / 2048.f;
-
-
-=======
 	Direction Direction;
 	const float PlayerTu = 90.f / 1024.f;
 	const float PlayerTv = 180.f / 2048.f;
 	float Speed;
 	float const tu = 90;
 	float const tv = 180;
-<<<<<<< HEAD
-=======
-=======
-
->>>>>>> [#11縲enh]繧ｾ繝ｳ繝薙・豁ｩ陦後い繝九Γ繝ｼ繧ｷ繝ｧ繝ｳ縲蟾ｦ蜿ｳ蜿崎ｻ｢縺励※繧・
->>>>>>> master
-
->>>>>>> 逕ｻ蜒上・繧｢繝九Γ繝ｼ繧ｷ繝ｧ繝ｳ・育判蜒上ｒ邨ｱ蜷医ヵ繧ｧ繧､繝ｫ縺ｫ螟画峩・・
 	KEYSTATE Key[KEYMAX] = { KEY_OFF };
 
 };
